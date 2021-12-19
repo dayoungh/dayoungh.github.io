39b7e617099303154a835a40ffbea2d91f89dd5b
dayoungh <91935129+dayoungh@users.noreply.github.com>
2021-12-19 22:34:27 +0900

    Add files via upload

---
+++ pf06/js/index.js
@@ -0,0 +1,34 @@
+$(function(){
+// -----------------------------------------------
+
+var main=$('#main').fullpage({
+    anchors:['con01','con02','con03','con04','con05','footer'],
+    css3:false,     //background-attachment :fixed 활성화,
+    // navigation:true,
+    afterLoad: function(origin, destination, direction){
+      var idx=destination.index;
+      console.log(idx);
+      $('.section').eq(idx).addClass('on').siblings().removeClass('on');
+      $('nav li').eq(idx).addClass('on').siblings().removeClass('on');
+     
+    },
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------
+});
