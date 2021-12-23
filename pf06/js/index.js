3eca7287c4e6c16ab6bce0bb15ea1ece5221c47e
dayoungh <91935129+dayoungh@users.noreply.github.com>
2021-12-23 12:02:44 +0900

    Add files via upload

--- pf06/js/index.js
+++ pf06/js/index.js
@@ -1,12 +1,14 @@
 $(function(){
   // -----------------------------------------------
   
-  
+ 
   
   var main=$('#main').fullpage({
       anchors:['con01','con02','con03','con04','con05','footer'],
       css3:false,     //background-attachment :fixed 활성화,
-      // navigation:true,
+      responsiveWidth: 769, 
+     
+    
       afterLoad: function(origin, destination, direction){
         var idx=destination.index;
         // console.log(idx);
@@ -14,8 +16,10 @@ $(function(){
         $('nav li').eq(idx).addClass('on').siblings().removeClass('on');
         $('.pg_nav li').eq(idx).addClass('on').siblings().removeClass('on');
         destination.index==0 ? $('.header').removeClass('on') : $('.header').addClass('on');
+        
+        
       },
-      responsiveWidth: 769, 
+      
   });
 
 
@@ -55,9 +59,96 @@ $(function(){
   
   
   
-  
-  
-  
+    // txt_slide //
+    $('.txt_slide').slick({
+      arrows:false,
+      dots:false,
+      slidesToShow:3,
+      autoplay:true,
+      autoplaySpeed:2500,
+      pauseOnHover:false,
+      pauseOnFocus:false,
+      responsive: [
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 1,
+            // dots:true,
+          }
+        },
+   
+      ]
+      
+    });
+
+    //scroll event //
+    const SEC = document.querySelectorAll('.action');
+    const WT=window.innerHeight;
+
+     window.addEventListener("scroll", ()=>{
+        let sct=window.scrollY;
+        SEC.forEach(e=>{
+            let secTop = e.offsetTop;
+            let secH=e.clientHeight;
+            sct > secTop -(WT-secH)/2 -200 ? e.classList.add('on') : e.classList.remove('on');
+        });
+     });
+
+
+   
+
+
+       // tab //
+      $('.tab_menu li').on('click',function(){
+        var idx=$(this).index();
+        // $('.tab_cont>div').eq(idx).show().siblings().hide();
+        $('.tab_cont>div').eq(idx).addClass('on').siblings().removeClass('on');
+       });
+    
+
+
+    // $('.tab_slider').on('afterChange', function(e,s,c){
+    //   $('.tab_menu li').eq(c).addClass('on').siblings().removeClass('on');
+        
+    // })
+
+
+    // video slider //
+    $(".video_slider").slick({
+      autoplay:true,
+      autoplaySpeed:3000,
+      pauseOnHover:false,
+      pauseOnFocus:false,
+      dots:true,
+      arrows:false,
+    
+  });
+
+
+    $('.con04 .arrows .xi-angle-left').on('click',function(){
+      $('.video_slider').slick('slickPrev')
+       });
+   
+    $('.con04 .arrows .xi-angle-right').on('click',function(){
+       $('.video_slider').slick('slickNext')
+      });
+
+
+      // 달력 //
+      $(function() {
+        $( "#datepicker" ).datepicker({
+            inline: true,
+            showOtherMonths: true,
+            showMonthAfterYear: true,
+            monthNames: [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12' ],
+            dayNamesMin: ['일', '월', '화', '수', '목', '금', '토']
+        });
+    });
+
+
+
+
+    
   
   
   
