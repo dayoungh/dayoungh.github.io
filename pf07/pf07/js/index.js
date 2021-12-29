89fc222cac5ff86787f5530397b553992ef1108b
dayoungh <91935129+dayoungh@users.noreply.github.com>
2021-12-29 19:20:04 +0900

    Add files via upload

---
+++ pf07/pf07/js/index.js
@@ -0,0 +1,27 @@
+$(function(){
+
+
+ // 반응형 메뉴 //
+// $('.nav-b>nav>.depth01>li a').on('click',function(){
+//     if($(window).width() < 769) {
+//         $(this).next().stop().slideToggle();
+//         $(this).parent().siblings().find('.depth02').slideUp();
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ---------------------------------------------
+});
