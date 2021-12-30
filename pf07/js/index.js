0845ead230706cd76cfc12c0a51e40d810d9fa4a
dayoungh <91935129+dayoungh@users.noreply.github.com>
2021-12-30 23:27:16 +0900

    Add files via upload

--- pf07/js/index.js
+++ pf07/js/index.js
@@ -1,18 +1,38 @@
 $(function(){
 
-
+    // mopen ///
+    $('.mopen').on('click',function(){
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+    });
   
    
-    // // nav ///
-    // $('.depth02>li>a').on('click',function(){
-    //     if($(window).width() < 769) {
-    //         $(this).next().stop().slideToggle();
-    //         $(this).parent().siblings().find('.depth03').slideUp();
-    //     }
-    // });
+    //  nav ///
+    $('.depth02>li>a').on('click',function(){
+        if($(window).width() < 769) {
+            $(this).next().stop().slideToggle();
+            $(this).parent().siblings().find('.depth03').slideUp();
+        }
+    });
+    
+    $('.depth01>li>a').on('click',function(){
+        if($(window).width() < 769) {
+            $(this).next().toggle();
+            $(this).parent().siblings().find('.depth03').hide();
+        }
+    });
    
    
-   
+      // visual video ///
+      $('#bgndVideo').YTPlayer({
+        videoURL:'https://youtu.be/NpW0iz52lVs',
+        containment:'#visual',
+        autoPlay:true, 
+        mute:true, 
+        startAt:0, 
+        opacity:1,
+        showControls:false,
+    });
    
    
    
