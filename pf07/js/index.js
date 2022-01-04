e9431021633217b987824eea8ed83f1fa864186b
dayoungh <91935129+dayoungh@users.noreply.github.com>
2022-01-04 23:33:40 +0900

    Add files via upload

--- pf07/js/index.js
+++ pf07/js/index.js
@@ -4,6 +4,7 @@ $(function(){
     $('.mopen').on('click',function(){
         $(this).toggleClass('on');
         $('.gnb').toggleClass('on');
+        $('#header').toggleClass('on');
     });
   
    
@@ -34,9 +35,82 @@ $(function(){
         opacity:1,
         showControls:false,
     });
+
+
+    // 공지사항 ticker_02 //
+    function tick2(){
+		$('#ticker_02 li:first').slideUp( function () { $(this).appendTo($('#ticker_02')).slideDown(); });
+	}
+	setInterval(function(){ tick2 () }, 2000);
+
    
-   
-   
+    // news slider //
+    $(".news_slider").slick({
+        autoplay:true,
+        arrows:false,
+        autoplaySpeed:3000,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+        dots:true,
+        // vertical:true,
+        responsive: [
+        {
+            breakpoint: 769,
+            settings: {
+            slidesToShow: 1,
+            // vertical:false,
+            }
+        },
+    
+        ]
+    });
+
+    // result slider //
+    $(".result_slider").slick({
+        autoplay:true,
+        arrows:false,
+        autoplaySpeed:3000,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+        dots:true,
+        vertical:true,
+        responsive: [
+        {
+            breakpoint: 769,
+            settings: {
+            slidesToShow: 1,
+            // vertical:false,
+            }
+        },
+    
+        ]
+    });
+
+
+
+
+
+
+
+
+
+
+
+    // to top
+    $('#scroll_banner li:nth-child(9)').on('click',function(){
+        $('html,body').animate({scrollTop:0},600);
+    });
+
+
+    // #scroll_banner
+    $(window).on('scroll',function(){
+        var sct=$(window).scrollTop();
+      
+        $('#scroll_banner').css({top:300+sct})
+    
+    });
+
+
    
    
    
